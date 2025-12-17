869a2acf7a4760d9e4f54b2272c3bf7f9a1cdc2a
OllyDI <choiseungho11@hanmail.net>
2025-12-17 20:21:21 +0900

    25.12.17

---
+++ section01/src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  
+  const router = useRouter();
+
+  // 이벤트를 통해 이동하는 페이지는 프리페칭 작동X
+  const onClickButton = () => {
+    router.push('/test');
+  }
+
+
+  // useEffect를 통해 router.prefech를 해주면 해당 페이지는 프리페칭됨
+  useEffect(() => {
+    router.prefetch('/test');
+  }, [])
+
+  return (
+    <>
+      <header>
+
+        <Link href={'/'}>index</Link>
+        <br/>
+        <Link href={'/search'} prefetch={false}>search</Link> {/* prefetch={false} 하면 프리페칭 작동X */}
+        <br/>
+        <Link href={'/book/1'}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  )
+}
