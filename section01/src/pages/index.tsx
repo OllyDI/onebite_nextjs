0771f177dcfdcc2ed1810a0a1dd89d8f012697ca
OllyDI <choiseungho11@hanmail.net>
2025-12-18 17:21:42 +0900

    25.12.18

--- section01/src/pages/index.tsx
+++ section01/src/pages/index.tsx
@@ -1,5 +1,11 @@
+// app.tsx 페이지를 제외하고는 글로벌 css를 임포트할 수 없음 -> css module 사용
+import style from './index.module.css'
+
 export default function Home() {
   return (
-    <h1>인덱스</h1>
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
   )
 }
