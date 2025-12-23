ae386cf5eb94d6bb3d945198887ac7ef48e14c18
OllyDI <choiseungho11@hanmail.net>
2025-12-23 19:39:40 +0900

    25.12.23

--- app_router/src/app/(with-searchbar)/page.tsx
+++ app_router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,59 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+/**
+ * Next.js fetch 메서드의 데이터 캐시 -> 기본 값이 캐싱하지 않음
+ * 1. cache: 'no-store': 캐싱하지 않음
+ * 2. cache: 'force-cache': 무조건 캐싱함 
+ * 3. next: { revalidate: X }: X 시간을 주기로 데이터 캐싱 -> ISR과 유사
+ * 4. next: { tags: ['a'] }: 요청이 들어왔을 때만 데이터 캐싱 -> On-Demand Revalidate(On-Demand ISR)
+ */
+
+async function AllBooks() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, 
+    { cache: 'no-store' }
+  );
+  if (!res.ok) return <div>오류가 발생했습니다 ...</div>;
+  const allBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => 
+        <BookItem key={book.id} {...book} />
+      )}
+    </div>
+  )
+}
+
+async function RecoBooks() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 10 } }
+  );
+  if (!res.ok) return <div>오류가 발생했습니다 ...</div>;
+  const recoBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => 
+      <BookItem key={book.id} {...book} />
+      )}
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- app_router/src/app/layout.tsx
+++ app_router/src/app/layout.tsx
@@ -1,6 +1,29 @@
+import { BookData } from "@/types";
 import "./globals.css";
 import style from "./layout.module.css";
 import Link from "next/link";
+
+/**
+ * Request Memoization
+ * /book api 처럼 한번의 요청에 여러 번 중복 호출되는 상황에 사용
+ * 다른 메서드 구현 없이 자동으로 동작함
+ */
+
+async function Footer() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!res.ok) return <footer>제작 ollyd</footer>;
+
+  const books: BookData[] = await res.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 ollyd</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  )
+}
+
 
 export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode;
@@ -12,7 +35,7 @@ export default function RootLayout({ children }: Readonly<{
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 ollyd</footer>
+          <Footer />
       </body>
     </html>
   );
