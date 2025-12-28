512ac02f518cda546bdaccff82e6cbbafae89ab2
OllyDI <choiseungho11@hanmail.net>
2025-12-28 13:55:31 +0900

    25.12.28

--- app_router/src/app/book/[id]/page.tsx
+++ app_router/src/app/book/[id]/page.tsx
@@ -1,7 +1,8 @@
-import { BookData } from "@/types";
+import { BookData, ReviewData } from "@/types";
 import style from "./page.module.css";
 import { notFound } from "next/navigation";
-
+import ReviewItem from "@/components/review-item";
+import ReviewEditor from "@/components/review-editor";
 
 /**
  * 동적 페이지로서 기본적으로 다이나믹 페이지로 설정됨
@@ -20,12 +21,9 @@ export function generateStaticParams() {
   return [{id: '1'}, {id: '2'}, {id: '3'}]
 }
 
-export default async function Page({ params, }: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-    
-  const { id } = await params;
-  
+
+async function BookDetail({ id }: { id: string }) {
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`);
   if (!res.ok) {
     if (res.status === 404) notFound();
@@ -35,8 +33,9 @@ export default async function Page({ params, }: {
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
+  
   return (
-    <div className={style.container}>
+    <section>
       <div className={style.cover_img_container} style={{ backgroundImage: `url('${coverImgUrl}')` }}>
         <img src={coverImgUrl} />
       </div>
@@ -44,6 +43,40 @@ export default async function Page({ params, }: {
       <div className={style.subTitle}>{subTitle}</div>
       <div className={style.author}>{author} | {publisher}</div>
       <div className={style.description}>{description}</div>
-    </div>
+    </section>
   );
+}
+
+
+async function ReviewList({ bookId }: { bookId: string }) {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`);
+
+  if (!res.ok) throw new Error(`Review fetch failed: ${res.statusText}`);
+
+  const reviews: ReviewData[] = await res.json();
+
+  return (
+    <section>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
+    </section>
+  )
+}
+
+
+export default async function Page({ params, }: {
+  params: Promise<{ id: string }>;
+}) {
+    
+  const { id } = await params;
+  
+  return (
+    <div className={style.container}>
+      <BookDetail id={id} />
+      <ReviewEditor bookId={id} />
+      <ReviewList bookId={id} />
+    </div>
+  )
 }
